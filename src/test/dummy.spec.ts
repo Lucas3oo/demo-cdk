b43cc61f7213c56e87632e279bd492765c28fcb6
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-06-08 21:14:37 +0200

    add dummy files to test sonarcloud

---
+++ src/test/dummy.spec.ts
@@ -0,0 +1,11 @@
+const expect = require('chai').expect
+
+describe('No assertion', function () {
+  it('doesnt test anything', function () { // Noncompliant
+    const str = ''
+    console.log(str)
+  })
+})
+
+
+
